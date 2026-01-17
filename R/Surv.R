0726902eb06ba2deaaf0b78f2298069d904c51ba
a1arakkal <alan-arakkal@uiowa.edu>
2026-01-17 09:36:18 -0700

    update documentation of Surv stop if not right censored or if time 2 is also supplied

--- R/Surv.R
+++ R/Surv.R
@@ -2,13 +2,19 @@
 #' 
 #' Create a survival object, usually used as a response variable in a 
 #' model formula. Argument matching is special for this function, see 
-#' Details under \code{survival::Surv}, as this is purely a wrapper for it.
+#' Details under \code{\link[survival]{Surv}}. This is a restricted wrapper around
+#' \code{\link[survival]{Surv}} and currently supports only right-censored data.
 #' 
 #' @param ... arguments to be passed into \code{survival::Surv}.  Currently, 
 #' the input must be of the form \code{Surv(time,event)} for right censored 
 #' data.
-#' 
+#' @return An object of class "\code{Surv}".
 #' @export
 Surv = function(...){
-  survival::Surv(...)
+  s <- survival::Surv(...)
+  # check if right censored
+  if (attr(s, "type") != "right" || ncol(s)>2) { 
+    stop("Only right-censored Surv objects are supported.") # stop if not right censored or if time 2 is also supplied
+  }
+  return(s)
 }
