a3e1664f59bd3502454e828aa650db886ab27a70
dksewell <dksewell@uiowa.edu>
2026-04-24 17:02:42 -0500

    Updated DIC and WAIC for just lm_b and aov_b

--- R/IC.R
+++ R/IC.R
@@ -10,7 +10,10 @@
 #' @param seed integer.  Always set your seed!!!
 #' @param mc_error The number of posterior draws will ensure that 
 #' with 99% probability the posterior mean of the deviance for DIC will be 
-#' within \eqn{\pm}\code{mc_error}E(deviance).
+#' within \eqn{\pm}\code{mc_error}.  For WAIC, this is based on 
+#' extrapolating the standard error from the preliminary 
+#' posterior samples and may be inaccurate (at least 2000 samples 
+#' will be used in final calculation).
 #' @param ... Passed to methods.
 #' 
 #' @returns Numeric (or in the case of DIC, a numeric vector)
@@ -142,7 +145,7 @@ DIC.lm_b = function(object,
     tcrossprod(X,as.matrix(theta_draws[,1:p]))
   ### Compute draws of phi
   if(ncol(theta_draws) > p){
-    phi = as.vector(unlist(theta_draws[,p]))
+    phi = as.vector(unlist(theta_draws[,p + 1]))
     if(object$family$family == "negbinom")
       phi = exp(phi)
   }else{
@@ -187,7 +190,7 @@ DIC.lm_b = function(object,
       )
     ### Compute draws of phi
     if(ncol(theta_draws) > p){
-      phi = as.vector(unlist(theta_draws[,p]))
+      phi = as.vector(unlist(theta_draws[,p + 1]))
       if(object$family$family == "negbinom")
         phi = exp(phi)
     }else{
@@ -291,66 +294,207 @@ DIC.aov_b = function(object, ...){
 #' @exportS3Method WAIC lm_b 
 WAIC.lm_b = function(object,
                      seed = 1,
+                     mc_error = 0.5,
                      ...){
   
   if(object$model_type == "nonparametric")
     stop("Cannot compute likelihood for a non-parametric fit.")
   
   set.seed(seed)
-  y = model.frame(object$formula,
-                  object$data)[,1]
-  X = model.matrix(object$formula,
-                   object$data)
   
-  n_draws = 1e4
-  p = nrow(object$summary)
-  n = nrow(X)
+  # Get log likelihood function
+  log_lik_function <- function(y, mu, phi = NULL) {
+    switch(object$family$family,
+           gaussian   = dnorm(y,
+                              mu,
+                              sqrt(phi),
+                              log = TRUE),
+           binomial   = dbinom(y,
+                               object$trials,
+                               mu/object$trials,
+                               log = TRUE),
+           poisson    = dpois(y,
+                              mu,
+                              log = TRUE),
+           negbinom   = dnbinom(y,
+                                mu = mu,
+                                size = phi,
+                                log = TRUE),
+           stop("Unsupported family")
+    )
+  }
   
-  V_tilde_eig = eigen(object$posterior_parameters$V_tilde)
-  if(ncol(X) > 1){
-    Vinv_sqrt = tcrossprod(diag(1 / sqrt(V_tilde_eig$values)),
-                           V_tilde_eig$vectors)
+  # Extract 
+  mframe = model.frame(terms(object),
+                       data = object$data)
+  
+  X = model.matrix(delete.response(terms(object)),
+                   data = object$data)
+  
+  os = model.offset(mframe)
+  N = nrow(X)
+  p = ncol(X)
+  if(is.null(os)) os = numeric(N)
+  
+  y = model.response(mframe)
+  if(is.character(y)) 
+    y = factor(y)
+  if(is.factor(y)){
+    y = as.integer(y)
+    if(length(unique(y)) == 2) y = y - 1
+  }
+  
+  if("trials" %in% names(object)){
+    trials = object$trials
   }else{
-    Vinv_sqrt = drop(V_tilde_eig$vectors) / sqrt(V_tilde_eig$values)
-  }
-  post_draws = 
-    matrix(0.0,n_draws,p + 1,
-           dimnames = list(NULL,
-                           c(object$summary$Variable,"s2")))
-  post_draws[,"s2"] = 
-    extraDistr::rinvgamma(n_draws,
-                          0.5 * object$posterior_parameters$a_tilde,
-                          0.5 * object$posterior_parameters$b_tilde)
-  post_draws[,1:p] = 
-    matrix(1.0,n_draws,1) %*% matrix(object$summary$`Post Mean`,nrow=1) +
-    matrix(rnorm(n_draws*p,
-                 sd = sqrt(rep(post_draws[,"s2"],p))),n_draws,p) %*% Vinv_sqrt
-  
-  betaX = tcrossprod(post_draws[,1:p],X)
-  lik_i = 
-    matrix(0.0,n_draws,n)
-  for(i in 1:n){
-    lik_i[,i] = 
-      dnorm(y[i],
-            mean = betaX[,i],
-            sd = sqrt(post_draws[,"s2"]))
+    trials = rep(1.0,N)
   }
   
-  lppd = 
-    lik_i |>
-    colMeans() |>
-    log() |>
-    sum()
+  # Create helper functions
+  ## Compute WAIC from matrix of individual log likelihood draws
+  waic_from_matrix = function(llik_sub){
+    
+    lik_sub <- exp(llik_sub)
+    
+    lppd_i =
+      apply(lik_sub, 1, function(x) log(mean(x)))
+    
+    # WAIC penalty
+    p_waic_i <- apply(llik_sub, 1, var)
+    
+    # WAIC
+    -2.0 * sum(lppd_i - p_waic_i)
+  }
   
-  p_waic2 = 
-    lik_i |>
-    log() |>
-    apply(2,var) |>
-    sum()
+  ## Estimate MC variance using batches
+  waic_mc_variance <- function(llik_i_draws,
+                               alpha = 0.2,
+                               R = 200){
+    
+    N <- nrow(llik_i_draws)
+    S <- ncol(llik_i_draws)
+    
+    m <- floor(alpha * S)
+    K <- floor(S / m)
+    
+    var_estimates <- numeric(R)
+    
+    for(r in 1:R){
+      # random permutation of draws
+      perm <- sample(S)
+      
+      waic_batches <- numeric(K)
+      
+      for(k in 1:K){
+        idx <- perm[((k - 1) * m + 1):(k * m)]
+        llik_sub <- llik_i_draws[, idx, drop = FALSE]
+        waic_batches[k] <- waic_from_matrix(llik_sub)
+      }
+      
+      var_estimates[r] <- var(waic_batches)
+    }
+    
+    list(
+      m = m,
+      K = K,
+      R = R,
+      mean_var = mean(var_estimates),
+      sd_var = sd(var_estimates),
+      all_var_estimates = var_estimates
+    )
+  }
   
-  -2.0 * (
-    lppd - p_waic2
-  )
+  
+  # Get posterior samples 
+  ## Get preliminary draws
+  
+  ### Get draws
+  theta_draws = 
+    get_posterior_draws(object,
+                        n_draws = 2000)
+  ### Compute draws of linear predictor
+  Xbeta_draws = 
+    tcrossprod(X,as.matrix(theta_draws[,1:p]))
+  ### Compute draws of phi
+  if(ncol(theta_draws) > p){
+    phi = as.vector(unlist(theta_draws[,p + 1]))
+    if(object$family$family == "negbinom")
+      phi = exp(phi)
+  }else{
+    phi = rep(1.0,2000)
+  }
+  ### Compute individual likelihood values
+  llik_i_draws = 
+    future.apply::future_sapply(1:nrow(theta_draws),
+                                function(i){
+                                  log_lik_function(y,
+                                                   trials *
+                                                     object$family$linkinv(eta = 
+                                                                             drop(Xbeta_draws[,i]) + 
+                                                                             os),
+                                                   phi[i])
+                                })
+  
+  
+  ### Estimate the variance of smaller batches
+  preliminary_results = 
+    waic_mc_variance(llik_i_draws)
+  
+  ### Pick the 90th percentile of variance estimates from all splits
+  conservative_est_of_mc_var = 
+    quantile(preliminary_results$all_var_estimates,0.9)
+  
+  ### Estimate the number of samples needed for accurate WAIC estimation
+  #     Note: This is unjustified extrapolation.  
+  n_prelim_draws = ncol(llik_i_draws)
+  n_draws = 
+    (0.2 * n_prelim_draws) *
+    conservative_est_of_mc_var *
+    qnorm(0.5 * (1.0 - 0.99))^2 /
+    mc_error^2
+  n_draws = round(n_draws)
+  
+  
+  ## Get remaining draws if needed.
+  if(n_draws > n_prelim_draws){
+    ### Get draws
+    theta_draws = 
+      rbind(theta_draws,
+            get_posterior_draws(object,
+                                n_draws = n_draws - n_prelim_draws)
+      )
+    ### Compute draws of linear predictor
+    Xbeta_draws = 
+      cbind(
+        Xbeta_draws,
+        tcrossprod(X,as.matrix(theta_draws[-c(1:n_prelim_draws),1:p]))
+      )
+    ### Compute draws of phi
+    if(ncol(theta_draws) > p){
+      phi = as.vector(unlist(theta_draws[,p + 1]))
+      if(object$family$family == "negbinom")
+        phi = exp(phi)
+    }else{
+      phi = rep(1.0,ncol(Xbeta_draws))
+    }
+    ### Compute individual likelihood values
+    llik_i_draws = 
+      cbind(
+        llik_i_draws,
+        future.apply::future_sapply((n_prelim_draws + 1):n_draws,
+                                    function(i){
+                                      log_lik_function(y,
+                                                       trials *
+                                                         object$family$linkinv(eta = 
+                                                                                 drop(Xbeta_draws[,i]) + 
+                                                                                 os),
+                                                       phi[i])
+                                    })
+      )
+  }
+  
+  # Compute WAIC
+  waic_from_matrix(llik_i_draws)
 }
 
 #' @rdname IC
@@ -417,87 +561,3 @@ WAIC.aov_b = function(object,
   
 }
 
-
-#' @rdname IC
-#' @exportS3Method WAIC glm_b 
-WAIC.glm_b = function(object,
-                      seed = 1,
-                      ...){
-  
-  if(object$model_type == "nonparametric")
-    stop("Cannot compute likelihood for a non-parametric fit.")
-  
-  set.seed(seed)
-  mframe = model.frame(object$formula, object$data)
-  y = model.response(mframe)
-  X = model.matrix(object$formula,object$data)
-  os = model.offset(mframe)
-  if(is.null(os)) os = numeric(nrow(object$data))
-  
-  n_draws = 1e4
-  
-  if("posterior_covariance" %in% names(object)){
-    post_draws = 
-      mvtnorm::rmvnorm(n_draws,
-                       mean = object$summary$`Post Mean`,
-                       sigma = object$posterior_covariance)
-  }else{#End: large sample approx
-    # If IS was used, use SIR
-    post_draws = 
-      object$proposal_draws[sample(1:NROW(object$importance_sampling_weights),
-                                   n_draws,
-                                   TRUE,
-                                   object$importance_sampling_weights),,drop=FALSE]
-    
-  }#End: IS approach
-  
-  mu = 
-    (os + tcrossprod(X,post_draws[,1:ncol(X)])) |> 
-    object$family$linkinv()
-  if(object$family$family == "poisson"){
-    llik_i = 
-      future.apply::future_sapply(1:nrow(object$data),
-                                  function(i){
-                                    dpois(y[i],
-                                          mu[i,],
-                                          log = TRUE)
-                                  })
-  }
-  if(object$family$family == "binomial"){
-    llik_i = 
-      future.apply::future_sapply(1:nrow(object$data),
-                                  function(i){
-                                    dbinom(y[i],
-                                           object$trials[i],
-                                           mu[i,],
-                                           log = TRUE)
-                                  })
-  }
-  if(object$family$family == "negbinom"){
-    phi = exp(post_draws[,ncol(X) + 1])
-    llik_i = 
-      future.apply::future_sapply(1:nrow(object$data),
-                                  function(i){
-                                    dnbinom(y[i],
-                                            mu = mu[i,],
-                                            size = phi[i],
-                                            log = TRUE)
-                                  })
-  }
-  
-  lppd = 
-    llik_i |>
-    exp() |> 
-    colMeans() |>
-    log() |>
-    sum()
-  
-  p_waic2 = 
-    llik_i |>
-    apply(2,var) |>
-    sum()
-  
-  -2.0 * (
-    lppd - p_waic2
-  )
-}
